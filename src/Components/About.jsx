4bb12c7a44ebb1495c6760c7ecb374b13c367caf
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-03-09 20:51:14 +0530

    feat: About section updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,19 +1,53 @@
 import React from 'react'
 import about_img from '../assets/my-img-1.jpg'
 
-const About = () => {
+const About = () => {  
   return (
     <div className='bg-black text-white py-20' id='about'>
         <div className='container mx-auto px-8 md:px-16 lg:px-24'>
-            <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
+            <h2 className='text-4xl font-bold text-center mb-12 text-shadow-blue'>About Me</h2>
             <div className='flex flex-col md:flex-row items-center md:space-x-12'>
                 <img src={about_img} alt="" className='w-72 rounded-4xl h-80 object-cover mb-8 md:mb-0'/>
-                <p className='text-lg mb-8'>Hi, I'm Thushanthan, a passionate Full-Stack Developer with a knack for building scalable, user-friendly applications. With expertise in front-end and back-end technologies, I specialize in crafting seamless digital experiences that bridge the gap between functionality and aesthetics.
+                <div className='flex-1'>
+                    <p className='text-lg mb-8'>Hi, I'm Thushanthan, a Full-Stack Developer passionate about building scalable and user-friendly applications. I specialize in front-end (React, Vue.js) and back-end (Node.js, Express, Django) development, with expertise in databases like MongoDB, PostgreSQL, and MySQL.
 
-                From designing intuitive React and Vue.js interfaces to developing robust Node.js, Express, and Django backends, I thrive in creating full-cycle web solutions. My skills extend to working with databases like MongoDB, PostgreSQL, and MySQL, ensuring optimized data management.
-
-                I’m always eager to explore new technologies, improve system architecture, and solve complex problems with clean, efficient code. Whether it’s developing an interactive web app, optimizing API performance, or deploying cloud-based solutions, I bring a problem-solving mindset and attention to detail to every project.
-                </p>
+                    I enjoy exploring new technologies, optimizing system architecture, and solving complex problems with clean, efficient code. Whether it's developing web apps, enhancing API performance, or deploying cloud solutions, I bring a problem-solving mindset and attention to detail to every project.
+                    </p>
+                    <div className='space-y-4'>
+                        <div className='flex items-center'>
+                          <label htmlFor="htmlandcss" className='w-2/12'>HTML & CSS</label>
+                          <div className='grow bg-gray-800 rounded-full h-2.5'>
+                              <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-11/12'></div>
+                          </div>
+                        </div>
+                        <div className='flex items-center'>
+                          <label htmlFor="htmlandcss" className='w-2/12'>React JS</label>
+                          <div className='grow bg-gray-800 rounded-full h-2.5'>
+                              <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-10/12'></div>
+                          </div>
+                        </div>
+                        <div className='flex items-center'>
+                          <label htmlFor="htmlandcss" className='w-2/12'>Node JS</label>
+                          <div className='grow bg-gray-800 rounded-full h-2.5'>
+                              <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-9/12'></div>
+                          </div>
+                        </div>
+                    </div>
+                    <div className='mt-12 flex justify-between text-center'>
+                      <div>
+                        <h3 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>3+</h3>
+                        <p className='font-bold text-shadow'>Years Experience</p>
+                      </div>
+                      <div>
+                        <h3 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>50+</h3>
+                        <p className='font-bold text-shadow'>Projects Completed</p>
+                      </div>
+                      <div>
+                        <h3 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>10+</h3>
+                        <p className='font-bold text-shadow'>Happy Clients</p>
+                      </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
